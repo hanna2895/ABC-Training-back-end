d8d813331b518dc39e1955f91f2bb6c33957f021
Hannah Werman <hanna.w28@gmail.com>
2018-05-24 15:26:30 -0500

    Switching database to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_05_24_195057) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "admins", force: :cascade do |t|
     t.string "name"
     t.string "email"
