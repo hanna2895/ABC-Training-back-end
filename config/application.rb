25c726a433c9375700575ba27fa43efe7ba74616
Hannah Werman <hanna.w28@gmail.com>
2018-05-30 16:53:46 -0500

    fixed cors to include credentials

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,16 @@ module Project4BackEnd
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins 'localhost:3001'
+    #     resource 'localhost:3001', headers: :any, methods: [:get, :post, :options],
+    #     credentials: true
+    #  end
+
+   # end
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -31,5 +41,7 @@ module Project4BackEnd
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+
   end
 end
