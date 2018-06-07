77d89858eef46e4eba12d1085a503d1dd86d9719
Hannah Werman <hanna.w28@gmail.com>
2018-06-07 14:27:14 -0500

    another try at changing cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://abc-training-1.herokuapp.com/'
+    origins 'abc-training-1.herokuapp.com'
     # origins '*'
 
     resource '*',
