cc80fe5194961cc87981311dbf222291faf6bc96
Hannah Werman <hanna.w28@gmail.com>
2018-06-10 12:34:33 -0500

    changed file model

--- app/models/file.rb
+++ app/models/file.rb
@@ -1,8 +1,8 @@
 class Files < ApplicationRecord
   belongs_to :group
   belongs_to :student
-  mount_uploader :file, FileUploader
-  has_many :documents
-  attr_accessor :document_data
+  # mount_uploader :file, FileUploader
+  # has_many :documents
+  # attr_accessor :document_data
   # has_one_attached :document
 end
